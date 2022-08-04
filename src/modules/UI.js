340b54c8ee3a83beb9f73c7145bf8ed3a174b75a
Danny Duong <danny.duong4.00@outlook.com>
2022-08-03 19:41:10 -0600

    Add project addition and deletion

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,8 +1,52 @@
 // import Project from './Project';
 import Todo from './Todo';
 import * as storage from './LocalStorage';
+import Project from './Project';
 
 export default class UI {
+  static createElement(tag, text, className = '') {
+    const el = document.createElement(tag);
+    el.innerText = text;
+    el.className = className;
+    return el;
+  }
+
+  static getTodoIndex(clickedEl) {
+    const liEl = clickedEl.parentNode.parentNode;
+    const todosEl = document.querySelector('.todos');
+    const index = Array.prototype.indexOf.call(todosEl.children, liEl);
+    return index;
+  }
+
+  static addTodoEvent(todo) {
+    todo.addEventListener('click', (e) => {
+      const { target } = e;
+      if (target === todo || target.classList.contains('summary') || target.classList.contains('title') || target.classList.contains('due-date') || target.classList.contains('details')) {
+        todo.classList.toggle('expanded');
+        todo.querySelector('.details').classList.toggle('hidden');
+      }
+    });
+  }
+
+  static addModalCloseEvents() {
+    const dialogs = document.querySelectorAll('.modal');
+    dialogs.forEach((dialog) => {
+      dialog.addEventListener('click', (e) => {
+        if (e.target === dialog) {
+          dialog.close();
+        }
+      });
+    });
+  }
+
+  static addNewProjectEvent() {
+    const newProjectEl = document.querySelector('.new-project');
+    newProjectEl.addEventListener('click', () => {
+      document.querySelector('.project-form').reset();
+      document.querySelector('.project.modal').showModal();
+    });
+  }
+
   constructor() {
     this.currTodoID = null;
     this.projects = storage.readProjects();
@@ -16,23 +60,30 @@ export default class UI {
   }
 
   populateProjectsHTML() {
-    const projectsEl = document.querySelector('.projects');
-
+    document.querySelector('.projects').innerHTML = '';
     this.projects.forEach((project) => {
-      const projectEl = UI.createElement('li', '', 'project');
-      projectEl.appendChild(UI.createElement('span', project.todos.length, 'todo-count'));
-      projectEl.appendChild(UI.createElement('span', project.name, 'name'));
-      projectEl.addEventListener('click', () => {
-        // Deselect other projects
-        projectEl.classList.add('selected');
-        this.currProject = project;
-        document.querySelector('.projects-header').innerHTML = project.name;
-      });
-      projectsEl.append(projectEl);
+      this.addProjectEl(project);
     });
   }
 
+  addProjectEl(project) {
+    const projectEl = UI.createElement('li', '', 'project');
+    projectEl.appendChild(UI.createElement('span', project.todos.length, 'todo-count'));
+    projectEl.appendChild(UI.createElement('span', project.name, 'name'));
+    const deleteEl = UI.createElement('img', '', 'delete');
+    deleteEl.src = '../../dist/images/delete-bin-line.svg';
+    projectEl.append(deleteEl);
+    this.addProjectDeleteEvent(deleteEl);
+    projectEl.addEventListener('click', () => {
+      this.currProject = project;
+      this.populateTodosHTML(this.currProject);
+    });
+    const projectsEl = document.querySelector('.projects');
+    projectsEl.append(projectEl);
+  }
+
   populateTodosHTML(project) {
+    document.querySelector('.projects-header').innerHTML = project.name;
     document.querySelector('.todos').innerHTML = '';
     for (let i = 0; i < project.todos.length; i += 1) {
       this.addTodoEl(project.todos[i], i);
@@ -41,8 +92,10 @@ export default class UI {
 
   addEventListeners() {
     this.addTodoButtonEvent();
-    UI.addModalCloseEvent();
-    this.addModalSubmitEvent();
+    UI.addModalCloseEvents();
+    this.addTodoModalSubmitEvent();
+    UI.addNewProjectEvent();
+    this.addProjectModalSubmitEvent();
   }
 
   addCheckboxEvent(checkbox) {
@@ -55,36 +108,27 @@ export default class UI {
     });
   }
 
-  static addTodoEvent(todo) {
-    todo.addEventListener('click', (e) => {
-      const { target } = e;
-      if (target === todo || target.classList.contains('summary') || target.classList.contains('title') || target.classList.contains('due-date') || target.classList.contains('details')) {
-        todo.classList.toggle('expanded');
-        todo.querySelector('.details').classList.toggle('hidden');
-      }
-    });
-  }
-
   addTodoButtonEvent() {
     const newTodo = document.querySelector('.new-todo-button');
     newTodo.addEventListener('click', () => {
       document.querySelector('.todo-form').reset();
-      document.querySelector('.add-text').innerText = 'New todo';
+      document.querySelector('.add-text').innerText = 'New Todo';
       this.currTodoID = this.currProject.todos.length;
-      document.querySelector('.new-todo-modal').showModal();
+      document.querySelector('.new-todo.modal').showModal();
     });
   }
 
-  static addModalCloseEvent() {
-    const dialog = document.querySelector('.new-todo-modal');
-    dialog.addEventListener('click', (e) => {
-      if (e.target === dialog) {
-        dialog.close();
-      }
+  addProjectModalSubmitEvent() {
+    document.querySelector('.project-form').addEventListener('click', () => {
+      const name = document.querySelector('#name').value;
+      const project = new Project(name);
+      this.addProjectEl(project);
+      this.projects.push(project);
+      storage.writeProjects(this.projects);
     });
   }
 
-  addModalSubmitEvent() {
+  addTodoModalSubmitEvent() {
     const todoForm = document.querySelector('.todo-form');
     todoForm.addEventListener('submit', () => {
       const title = document.querySelector('#title').value;
@@ -92,8 +136,6 @@ export default class UI {
       const dueDate = document.querySelector('#due-date').value;
       const priority = document.querySelector('input[name="priority"]:checked').value;
       const todo = new Todo(title, details, dueDate, priority);
-      storage.writeProjects(this.projects);
-      console.log(this.currTodoID);
       this.addTodoEl(todo, this.currTodoID);
     });
   }
@@ -123,8 +165,7 @@ export default class UI {
 
     const todosEl = document.querySelector('.todos');
     const todoEls = document.querySelector('.todos').children;
-    // console.log(i);
-    // console.log(todoEls[i]);
+
     if (i === todoEls.length) {
       todosEl.append(todoEl);
     } else {
@@ -138,7 +179,7 @@ export default class UI {
 
     this.addCheckboxEvent(checkboxEl);
     this.addEditEvent(editEl);
-    this.addDeleteEvent(deleteEl);
+    this.addTodoDeleteEvent(deleteEl);
     UI.addTodoEvent(todoEl);
   }
 
@@ -150,30 +191,27 @@ export default class UI {
       document.querySelector('#description').value = this.currProject.todos[this.currTodoID].description;
       document.querySelector('#due-date').value = this.currProject.todos[this.currTodoID].dueDate;
       document.querySelector(`#${this.currProject.todos[this.currTodoID].priority}`).checked = true;
-      document.querySelector('.new-todo-modal').showModal();
+      document.querySelector('.new-todo.modal').showModal();
     });
   }
 
-  addDeleteEvent(deleteEl) {
+  addProjectDeleteEvent(deleteEl) {
+    deleteEl.addEventListener('click', (e) => {
+      e.stopPropagation();
+      const index = Array.prototype.indexOf.call(document.querySelector('.projects').children, deleteEl.parentNode);
+      deleteEl.parentNode.remove();
+      this.projects.splice(index, 1);
+      storage.writeProjects(this.projects);
+      this.populateTodosHTML(this.projects[index - 1]);
+    });
+  }
+
+  addTodoDeleteEvent(deleteEl) {
     deleteEl.addEventListener('click', () => {
       const index = UI.getTodoIndex(deleteEl);
       this.currProject.todos.splice(index, 1);
       deleteEl.parentNode.parentNode.remove();
       storage.writeProjects(this.projects);
     });
-  }
-
-  static createElement(tag, text, className = '') {
-    const el = document.createElement(tag);
-    el.innerText = text;
-    el.className = className;
-    return el;
-  }
-
-  static getTodoIndex(clickedEl) {
-    const liEl = clickedEl.parentNode.parentNode;
-    const todosEl = document.querySelector('.todos');
-    const index = Array.prototype.indexOf.call(todosEl.children, liEl);
-    return index;
   }
 }
