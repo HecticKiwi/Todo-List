3da4e53ccb2198218de08d8b1ade150eb05bf9cb
Danny Duong <danny.duong4.00@outlook.com>
2022-08-04 19:47:11 -0600

    Add default todos

--- src/modules/LocalStorage.js
+++ src/modules/LocalStorage.js
@@ -1,5 +1,5 @@
 import Project from './Project';
-// import Todo from './Todo';
+import Todo from './Todo';
 
 export function readProjects() {
   let projects = [];
@@ -7,7 +7,11 @@ export function readProjects() {
     projects = JSON.parse(localStorage.getItem('projects'));
     projects = projects.map(({ name, todos }) => new Project(name, todos));
   } else {
-    projects.push(new Project('Default'));
+    const defaultProject = new Project('Default');
+    defaultProject.addTodo(new Todo('Learn about webpack', 'Especially look into configuration and loaders... tricky stuff.', '2022-08-20', 'low'));
+    defaultProject.addTodo(new Todo("Check out michalosman's project submissions on The Odin Project", 'His projects are consistently top-loved. I could learn a thing or two from his source code.', '2022-09-10', 'medium'));
+    defaultProject.addTodo(new Todo('Contribute to an open source project', 'I bet this would look good an a resume.', '2023-02-30', 'high'));
+    projects.push(defaultProject);
     writeProjects(projects);
   }
   return projects;
